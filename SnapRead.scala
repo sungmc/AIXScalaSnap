349046a595bf55fa03e9fbef324acf3dc5d49a57
Sung Chun <sungmc@us.ibm.com>
2017-08-16 19:55:17 -0500

    Added 'Please wait' so that we can wait for all reads to complete

--- SnapRead.scala
+++ SnapRead.scala
@@ -11,25 +11,42 @@ object SnapRead extends App {
   import java.time._
   import scala.concurrent._
   import ExecutionContext.Implicits.global
+  import scala.util.{ Failure, Success }
 
   val dir = FileSystems.getDefault.getPath("snap")
 
-  for (f <- Files.walk(dir).iterator().asScala.filter(f => f.toFile().isFile())) { // No directory  
-    //println("File is " + f)
-    if (f.toString().endsWith("snap")) // Read in *.snap file only
-      Future {
-        createCommands(FileToCommand_v2(f.toString()), dir.toString())
-      }
+  val future1 = Future {
+    for (f <- Files.walk(dir).iterator().asScala.filter(f => f.toFile().isFile())) { // No directory  
+      //println("File is " + f)
+      if (f.toString().endsWith("snap")) // Read in *.snap file only
+        // To Do: Some big file takes a while. 
+        Future {
+          createCommands(FileToCommand_v2(f.toString()), dir.toString())
+        }
+    }
+  }
+  var complete: Boolean = false
+  future1.onComplete {
+    case Success(value) => complete = true
+    case Failure(e)     => e.printStackTrace
+  }
+
+  print("Reading snap. Please wait .")
+  while (!complete) {
+    print("."); Thread.sleep(500)
   }
 
   println(welcomeMessage)
-  var line:String = ""
-  //var cont:Boolean = true
-  while (!line.equals("quit")) {
-    // readLine lets you prompt the user and read their input as a String
+  var line: String = ""
+  var cont: Boolean = true
+
+  while (cont) {
     do {
       line = scala.io.StdIn.readLine("$ ")
     } while (line.isEmpty())
-    println(SnapUtilies.cmdMap.get(line).getOrElse(None))  
+    line match {
+      case "quit" => println("Terminating vmsnap"); cont = false
+      case _ => println(SnapUtilies.cmdMap.get(line).getOrElse(None))
+    }
   }
 }
